bc85ae6801fd5c8fde3c27f6eb0114f8c9b40a7d
Kyösti Komulainen <kkomulainen@gmail.com>
2015-03-04 14:51:49 +0200

    Use array for TextFields to create UI

--- Task1/src/main/java/com/mycompany/helloworld/MyUI.java
+++ Task1/src/main/java/com/mycompany/helloworld/MyUI.java
@@ -1,7 +1,5 @@
 package com.mycompany.helloworld;
 
-import com.mycompany.views.FirstView;
-import com.mycompany.views.SecondView;
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
@@ -11,28 +9,16 @@ import com.vaadin.data.Item;
 import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.event.ItemClickEvent;
 import com.vaadin.event.ItemClickEvent.ItemClickListener;
-import com.vaadin.navigator.Navigator;
-import com.vaadin.navigator.Navigator.ComponentContainerViewDisplay;
-import com.vaadin.server.ExternalResource;
-import com.vaadin.server.FileResource;
-import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.shared.ui.MarginInfo;
-import com.vaadin.ui.AbstractComponent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.Embedded;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Link;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-import java.io.Console;
-import java.io.File;
 import java.util.Iterator;
 
 /**
@@ -43,12 +29,10 @@ import java.util.Iterator;
 public class MyUI extends UI implements Button.ClickListener {
 
     private HorizontalLayout mainLayout = new HorizontalLayout();
-    private VerticalLayout txtFields = new VerticalLayout();
-    private TextField fname = new TextField("Firstname");
-    private TextField lname = new TextField("Lastname");
-    private TextField address = new TextField("Address");
-    private TextField email = new TextField("Email");
-    private TextField phone = new TextField("Phonenumber");
+    private VerticalLayout lblVertical = new VerticalLayout();
+    private String[] labels = new String[] {"Firstname","Lastname","Address","Email","Phone"};
+    private TextField[] txtFields = new TextField[labels.length];
+
     private Button addButton = new Button("Add Contact");
     private Table contactList = new Table();
     
@@ -57,22 +41,21 @@ public class MyUI extends UI implements Button.ClickListener {
         Notification.show("Button pressed");
     }
 
-    
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         this.setContent(mainLayout);
         
-        txtFields.addComponent(fname);
-        txtFields.addComponent(lname);
-        txtFields.addComponent(address);
-        txtFields.addComponent(email);
-        txtFields.addComponent(phone);
-        txtFields.addComponent(addButton);
+        for(int i=0; i<labels.length; i++) {
+            txtFields[i] = new TextField(labels[i]);
+            lblVertical.addComponent(txtFields[i]);
+        }
+       
+        lblVertical.addComponent(addButton);
         
-        txtFields.setMargin(true);
-        txtFields.setSpacing(true);
+        lblVertical.setMargin(true);
+        lblVertical.setSpacing(true);
         
-        mainLayout.addComponent(txtFields);
+        mainLayout.addComponent(lblVertical);
         mainLayout.addComponent(contactList);
         
         // Alternative implementation for addButton event handler
@@ -82,8 +65,9 @@ public class MyUI extends UI implements Button.ClickListener {
         addButton.addClickListener(new Button.ClickListener() {
             @Override
             public void buttonClick(Button.ClickEvent event) {
+                
                 StringBuilder errMessage = new StringBuilder();
-                Iterator<Component> iter = txtFields.iterator();
+                Iterator<Component> iter = lblVertical.iterator();
                 while(iter.hasNext()) {
                     Component c = iter.next();
                     if(c instanceof TextField) {
@@ -98,11 +82,9 @@ public class MyUI extends UI implements Button.ClickListener {
                 
                 if(err.isEmpty()) {
                     addItemToTable();
-                    fname.setValue("");
-                    lname.setValue("");
-                    address.setValue("");
-                    phone.setValue("");
-                    email.setValue("");
+                    for(TextField t: txtFields) {
+                        t.setValue("");
+                    }
                 }
                 else {
                     Notification.show(err + "field(s) empty");
@@ -130,22 +112,19 @@ public class MyUI extends UI implements Button.ClickListener {
      
         Object objectId = contactList.addItem();
         Item row = contactList.getItem(objectId);
-        row.getItemProperty("Firstname").setValue(fname.getValue());
-        row.getItemProperty("Lastname").setValue(lname.getValue());
-        row.getItemProperty("Address").setValue(address.getValue());
-        row.getItemProperty("Email").setValue(email.getValue());
-        row.getItemProperty("Phone").setValue(phone.getValue());
+        
+        for(int i=0; i<labels.length; i++) {
+            row.getItemProperty(labels[i]).setValue(txtFields[i].getValue());
+        }
     }
     
     private IndexedContainer createDummyDataSource() {
         
         IndexedContainer ic = new IndexedContainer();
-        ic.addContainerProperty("Firstname", String.class, "");
-        ic.addContainerProperty("Lastname", String.class, "");
-        ic.addContainerProperty("Address", String.class, "");
-        ic.addContainerProperty("Email", String.class, "");
-        ic.addContainerProperty("Phone", String.class, "");
-        return ic;
+        for(int i=0; i<labels.length; i++) {
+            ic.addContainerProperty(labels[i], String.class, "");
+        }
+         return ic;
     }
     
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
