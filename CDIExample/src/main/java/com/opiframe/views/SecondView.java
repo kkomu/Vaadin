19b44146b26d240b29cc3689eb8ffade47b309a3
Kyösti Komulainen <kkomulainen@gmail.com>
2015-03-05 13:59:43 +0200

    Add table with data to SecondView

--- CDIExample/src/main/java/com/opiframe/views/SecondView.java
+++ CDIExample/src/main/java/com/opiframe/views/SecondView.java
@@ -5,11 +5,17 @@
  */
 package com.opiframe.views;
 
+import com.opiframe.entity.MicroMarket;
+import com.opiframe.session.MicroMarketFacade;
 import com.vaadin.cdi.CDIView;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
+import javax.inject.Inject;
 
 /**
  *
@@ -17,10 +23,20 @@ import com.vaadin.ui.Label;
  */
 @CDIView("SecondView")
 public class SecondView extends HorizontalLayout implements View {
+    
+    @Inject
+    MicroMarketFacade microMarket;
 
+    Table table = new Table();
+    
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
-        this.addComponent(new Label("This is the second view"));
+        BeanItemContainer bean = new BeanItemContainer(MicroMarket.class);
+        bean.addAll(microMarket.findAll());
+        
+        table.setContainerDataSource(bean);
+        table.setVisibleColumns(new Object[] {"radius","zipCode"});
+        table.setColumnHeaders(new String[] {"Radius","ZIP code"});
+        this.addComponent(table);
     }
-    
 }
